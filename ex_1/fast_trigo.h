49687726a70d470da803980a3cae7ec9ffa597d6
FedericaMoini <133591817+FedericaMoini@users.noreply.github.com>
2025-02-28 17:50:26 +0100

    Esercizi lezione 28/02/2025

---
+++ ex_1/fast_trigo.h
@@ -0,0 +1,11 @@
+#ifndef FAST_TRIGO_H
+#define FAST_TRIGO_H
+
+// No include guards! Will cause multiple inclusion errors.
+#include <math.h>
+
+// Function's declaration
+float fast_sin(float x);
+float fast_cos(float x);
+
+#endif // FAST_TRIGO_H
